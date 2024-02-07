39412483b73abc6a04e2b3cce2d58c020ff655c5
Threonaum <lepolak01@gmail.com>
2024-02-07 11:33:21 +0100

    corrections de bug by kevin

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php include "app/app.php"; $page = 1; head($page); ?>
 <body> <header> <?php navbar($page); ?> </header>
 
-<<<<<<< Updated upstream
-<main id="page-home">
-=======
-<<<<<<< HEAD
 <main id="home">
-=======
-<main id="page-home">
->>>>>>> d4234a2986dd300709fb6a0e04b164f96a303d6f
->>>>>>> Stashed changes
 
     <p>Home </p>
 
