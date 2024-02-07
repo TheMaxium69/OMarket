aa4cc475a2ee767f487e3134c393ab70981cc9bd
Maxime Tournier <mxm.tournier@gmail.com>
2024-02-07 14:22:14 +0100

    form

--- form.php
+++ form.php
@@ -1,11 +1,64 @@
 <?php include "app/app.php"; $page = 3; head($page); ?>
-<body> <header> <?php navbar($page); ?> </header>
+<body id="formBody">
+
+<!--<header> --><?php //navbar($page); ?><!-- </header>-->
 
 <main id="form">
 
-    <p>Form </p>
+
+    <?php
+
+        if (empty($_GET['q'])){
+
+    ?>
+
+        <h1>1) <?= $env_question[1][$ENV_LANG]["question"] ?> </h1>
+        <small><?= $env_question[1][$ENV_LANG]["sub_question"] ?></small>
+
+
+
+
+    <?php
+
+        } else {
+
+            if
+
+
+
+
+
+        }
+
+    ?>
+
+
 
 </main>
 
+<style>
+
+    #formBody {
+        background-color: #f6f2e8;
+        width: 100%;
+        height: 100%;
+    }
+
+    #form{
+        padding-left: 16%;
+        padding-top: 20%;
+    }
+
+    /*Question*/
+    #formBody h1{
+
+        font-weight: bold;
+        text-align: left;
+
+    }
+
+
+
+</style>
 
 </body> </html>
