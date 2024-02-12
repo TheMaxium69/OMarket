4fdfee428ad030b6bbf1a3e213086581759ddd96
Threonaum <lepolak01@gmail.com>
2024-02-12 15:48:55 +0100

    creation term du site
    term.php
    term.css

---
+++ term.php
@@ -0,0 +1,91 @@
+<?php include "app/app.php"; $page = 7; head($page); ?>
+<body> <header> <?php navbar($page); ?> </header>
+
+<main id="page-term">
+
+<h2>More Informations</h2>
+<h3>Find all the addtional information here to Omarket</h3>
+    <h4>Company Administration</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+        </p>
+
+    <h4>Thanks</h4>
+
+    <ul>
+        <li>
+            <p>Personne 1</p>
+        </li>
+        <li>
+            <p>Personne 2</p>
+        </li>
+        <li>
+            <p>Personne 3</p>
+        </li>
+        <li>
+            <p>Personne 4</p>
+        </li>
+        <li>
+            <p>Personne 5</p>
+        </li>
+        <li>
+            <p>Personne 1</p>
+        </li>
+    </ul>
+
+    <h4>technologies</h4>
+
+    <ul>
+        <li>
+            <p>Technology 1</p>
+        </li>
+        <li>
+            <p>Technology 2</p>
+        </li>
+        <li>
+            <p>Technology 3</p>
+        </li>
+        <li>
+            <p>Technology 4</p>
+        </li>
+        <li>
+            <p>Technology 5</p>
+        </li>
+        <li>
+            <p>Technology 6</p>
+        </li>
+    </ul>
+
+    <h4>Terms Of Service</h4>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+        </p>
+
+</main>
+
+<?php  footer(); ?>
+
+</body> </html>
