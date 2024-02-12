2dd25e3e57d6ebdf604db9652d08bcaa8025b33b
Threonaum <lepolak01@gmail.com>
2024-02-12 16:09:01 +0100

    update term
    creation du css
    mise en page theorique temporaire

--- term.php
+++ term.php
@@ -2,87 +2,98 @@
 <body> <header> <?php navbar($page); ?> </header>
 
 <main id="page-term">
+<section id="term">
+    <div class="informations">
+        <h2>More Informations</h2>
+            <h3>Find all the addtional information here to Omarket</h3>
+            <div id="company">
+                <h4>Company Administration</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                    </p>
+            </div>
+            <div id="thanks">
 
-<h2>More Informations</h2>
-<h3>Find all the addtional information here to Omarket</h3>
-    <h4>Company Administration</h4>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-        </p>
+                <h4>Thanks</h4>
 
-    <h4>Thanks</h4>
+                <ul>
+                    <li>
+                        <p>Person 1</p>
+                    </li>
+                    <li>
+                        <p>Person 2</p>
+                    </li>
+                    <li>
+                        <p>Person 3</p>
+                    </li>
+                    <li>
+                        <p>Person 4</p>
+                    </li>
+                    <li>
+                        <p>Person 5</p>
+                    </li>
+                    <li>
+                        <p>Person 6</p>
+                    </li>
+                </ul>
+            </div>
+            <div id="technology">
 
-    <ul>
-        <li>
-            <p>Personne 1</p>
-        </li>
-        <li>
-            <p>Personne 2</p>
-        </li>
-        <li>
-            <p>Personne 3</p>
-        </li>
-        <li>
-            <p>Personne 4</p>
-        </li>
-        <li>
-            <p>Personne 5</p>
-        </li>
-        <li>
-            <p>Personne 1</p>
-        </li>
-    </ul>
+                <h4>technologies</h4>
 
-    <h4>technologies</h4>
+                <ul>
+                    <li>
+                        <p>Technology 1</p>
+                    </li>
+                    <li>
+                        <p>Technology 2</p>
+                    </li>
+                    <li>
+                        <p>Technology 3</p>
+                    </li>
+                    <li>
+                        <p>Technology 4</p>
+                    </li>
+                    <li>
+                        <p>Technology 5</p>
+                    </li>
+                    <li>
+                        <p>Technology 6</p>
+                    </li>
+                </ul>
+            </div>
+            <div id="terms">
 
-    <ul>
-        <li>
-            <p>Technology 1</p>
-        </li>
-        <li>
-            <p>Technology 2</p>
-        </li>
-        <li>
-            <p>Technology 3</p>
-        </li>
-        <li>
-            <p>Technology 4</p>
-        </li>
-        <li>
-            <p>Technology 5</p>
-        </li>
-        <li>
-            <p>Technology 6</p>
-        </li>
-    </ul>
+                <h4>Terms Of Service</h4>
 
-    <h4>Terms Of Service</h4>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
-            tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
-        </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repellendus suscipit, ut ullam impedit deleniti id inventore doloribus eos numquam, 
+                        tempora, labore voluptatum. Rerum, optio. Ullam numquam modi soluta quas tempora
+                    </p>
+            </div>
+    </div>
+</section>
 
 </main>
 
